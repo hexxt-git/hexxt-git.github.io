c18a6af69fa04743a46f0d8f3ca3f8c71c0f63c2
9EED <88392191+9EED@users.noreply.github.com>
2023-11-04 02:59:38 +0100

    add last projects and finished up

--- src/script.js
+++
@@ -1,6 +0,0 @@
-let scroller = document.getElementById('landing-scroller')
-let Height = window.Height - 66 // todo do this
-
-scroller.addEventListener('click', ()=>{
-    window.scrollTo(Height)
-})
